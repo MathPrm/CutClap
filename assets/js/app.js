1fd429f669a4a99dd13a25bf15964f1f8917d520
MathPrm <primotmathilde@gmail.com>
2024-03-07 18:14:46 +0100

    18:14

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,47 +3,61 @@
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
 
-    const wiperTrack = document.querySelector(".wiper-track");
-    const wipes = Array.from(wiperTrack.children);
-    const wipePrevBtn = document.querySelector(".wiper-button__right");
-    const wipeNextBtn = document.querySelector(".wiper-button__left");
-    const wipeWidth = wipes[0].getBoundingClientRect().width;
+    // const wiperTrack = document.querySelector(".wiper-track");
+    // const wipes = Array.from(wiperTrack.children);
+    // const wipePrevBtn = document.querySelector(".wiper-button__right");
+    // const wipeNextBtn = document.querySelector(".wiper-button__left");
+    // const wipeWidth = wipes[0].getBoundingClientRect().width;
 
-    const arrowsBehaviour = (wipePrevBtn, wipeNextBtn, index) => {
-    if (index === 0) {
-        wipePrevBtn.classList.add("is-hidden");
-        wipeNextBtn.classList.remove("is-hidden");
-    } else if (index === wipes.length-1) {
-        wipePrevBtn.classList.remove("is-hidden");
-        wipeNextBtn.classList.add("is-hidden");
-    } else {
-        wipePrevBtn.classList.remove("is-hidden");
-        wipeNextBtn.classList.remove("is-hidden");
-    }
-    };
+    // const arrowsBehaviour = (wipePrevBtn, wipeNextBtn, index) => {
+    // if (index === 0) {
+    //     wipePrevBtn.classList.add("is-hidden");
+    //     wipeNextBtn.classList.remove("is-hidden");
+    // } else if (index === wipes.length-1) {
+    //     wipePrevBtn.classList.remove("is-hidden");
+    //     wipeNextBtn.classList.add("is-hidden");
+    // } else {
+    //     wipePrevBtn.classList.remove("is-hidden");
+    //     wipeNextBtn.classList.remove("is-hidden");
+    // }
+    // };
 
-    const wipeSlide = (wiperTrack, activeSlide, nextSlide, targetIndex) => {
-    wiperTrack.style.transform =
-        "translateX(-" + (wipeWidth + 24) * (targetIndex - 1) + "px)";
-    activeSlide.classList.remove("active-swipe");
-    activeSlide.style.transform = "scale(1)";
-    nextSlide.classList.add("active-swipe");
-    nextSlide.style.transform = "scale(1.1)";
-    };
+    // const wipeSlide = (wiperTrack, activeSlide, nextSlide, targetIndex) => {
+    // wiperTrack.style.transform =
+    //     "translateX(-" + (wipeWidth + 24) * (targetIndex - 1) + "px)";
+    // activeSlide.classList.remove("active-swipe");
+    // activeSlide.style.transform = "scale(1)";
+    // nextSlide.classList.add("active-swipe");
+    // nextSlide.style.transform = "scale(1.1)";
+    // };
 
-    wipeNextBtn.addEventListener("click", (e) => {
-    const activeSlide = wiperTrack.querySelector(".active-swipe");
-    const nextSlide = activeSlide.nextElementSibling;
-    const targetIndex = wipes.findIndex((slide) => slide === nextSlide);
-    wipeSlide(wiperTrack, activeSlide, nextSlide, targetIndex);
-    arrowsBehaviour(wipePrevBtn, wipeNextBtn, targetIndex);
-    });
-    wipePrevBtn.addEventListener("click", (e) => {
-    const activeSlide = wiperTrack.querySelector(".active-swipe");
-    const nextSlide = activeSlide.previousElementSibling;
-    const targetIndex = wipes.findIndex((slide) => slide === nextSlide);
-    wipeSlide(wiperTrack, activeSlide, nextSlide, targetIndex);
-    arrowsBehaviour(wipePrevBtn, wipeNextBtn, targetIndex);
-    });
+    // wipeNextBtn.addEventListener("click", (e) => {
+    // const activeSlide = wiperTrack.querySelector(".active-swipe");
+    // const nextSlide = activeSlide.nextElementSibling;
+    // const targetIndex = wipes.findIndex((slide) => slide === nextSlide);
+    // wipeSlide(wiperTrack, activeSlide, nextSlide, targetIndex);
+    // arrowsBehaviour(wipePrevBtn, wipeNextBtn, targetIndex);
+    // });
+    // wipePrevBtn.addEventListener("click", (e) => {
+    // const activeSlide = wiperTrack.querySelector(".active-swipe");
+    // const nextSlide = activeSlide.previousElementSibling;
+    // const targetIndex = wipes.findIndex((slide) => slide === nextSlide);
+    // wipeSlide(wiperTrack, activeSlide, nextSlide, targetIndex);
+    // arrowsBehaviour(wipePrevBtn, wipeNextBtn, targetIndex);
+    // });
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
 
 })
